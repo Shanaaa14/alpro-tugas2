f2244aaa8f40ecafb4c083c326b1109b94766074
Virza Shahnara <virzashahnara14@gmail.com>
2024-10-11 19:54:50 +0700

    Add files via upload

---
+++ jawaban.py
@@ -0,0 +1,42 @@
+print("TIPE DATA PADA PYTHON")
+
+# 1. Integer
+integer_value = 1,2,3,4,5,  
+print("1. Integer : Tipe data yang menyatakan bilangan bulat")
+print("  Contoh Nilai: ", integer_value, "dan seterusnya")
+print()
+
+# 2. Float
+float_value = 1.5, 2.75, 12.375,
+print("2. Float : Tipe data yang menyatakan bilangan desimal" )
+print("  Contoh Nilai: ", float_value, "dan seterusnya")
+print()
+
+# 3. Hexadecimal
+hex_value = "9a", "1d3"
+print("3. Hexadecimal : Tipe data yang menyatakan bilangan dalam format heksa (bilangan berbasis 16)" )
+print("  Contoh Nilai: ", hex_value,)
+print()
+
+# 4. Complex
+complex_value =  1+5j 
+print("4. Complex : Tipe data yang menyatakan bilangan dalam format imajiner" )
+print("  Contoh Nilai: ", complex_value,)
+print()
+
+# 5. Boolean
+boolean_value_true =  True
+boolean_value_false = False
+print("5. Boolean : Tipe data yang menyatakan nilai kebenaran yaitu True dan False" )
+print("   Nilai True: ", boolean_value_true,)
+print("   Nilai False: ", boolean_value_false)
+print()
+
+# 6. String
+string_value_kalimat = "Selamat Pagi",
+string_value_angka = "20"
+print("6. String : Tipe data yang menyatakan kalimat atau karakter dan bisa berupa angka atau huruf ")
+print("   Contoh kalimat: ", string_value_kalimat)
+print("   Contoh angka: ", string_value_angka)
+print()
+
